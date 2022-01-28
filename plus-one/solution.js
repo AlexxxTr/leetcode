cda9df8337d10ca0e55c00ae7712cb53d772391f
Alexander Truyaert <alexander.truyaert@outlook.com>
2022-01-28 14:30:16 +0100

    Added solution

---
+++ plus-one/solution.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+const plusOne = function (digits) {
+    let num = BigInt(digits.join('')) + BigInt(1);
+    return num.toString().split('').map(Number);
+};

0c012984b62188cf186631832edbd9c70eb72859
Alexander Truyaert <alexander.truyaert@outlook.com>
2022-01-30 12:46:16 +0100

    Added new solution for fibonacci

--- fibonnaci-number/solution.ts
+++ fibonnaci-number/solution.ts
@@ -3,3 +3,13 @@ function fib(n: number): number {
     if (n === 1) return 1;
     return fib(n - 1) + fib(n - 2);
 };
+
+function fib2(n: number): number {
+    let array: Array<number> = Array.from({ length: n + 1 }, (_, i) => 0);
+    array[1] = 1;
+
+    for (let i = 2; i <= n; i++)
+        array[i] = array[i - 1] + array[i - 2];
+
+    return array[n]
+}

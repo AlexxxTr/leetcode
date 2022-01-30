0def7ccf2e8c38f53444821ee18c1f3c17b9ea65
Alexander Truyaert <alexander.truyaert@outlook.com>
2022-01-30 21:46:45 +0100

    Added 2nd solution rotate array

--- rotate-array/solution.ts
+++ rotate-array/solution.ts
@@ -7,3 +7,18 @@ function rotate(nums: number[], k: number): void {
         nums.unshift(number);
     }
 };
+
+function rotate2(nums: number[], k: number): void {
+    k = k % nums.length;
+    reverse(0, nums.length - 1, nums);
+    reverse(0, k - 1, nums);
+    reverse(k, nums.length - 1, nums);
+}
+
+let reverse = (l: number, r: number, nums: number[]) => {
+    while (l < r) {
+        [nums[l], nums[r]] = [nums[r], nums[l]];
+        l++;
+        r--;
+    }
+}

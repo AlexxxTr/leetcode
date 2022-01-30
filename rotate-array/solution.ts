bd0e4f9be6e52316e345292c229ccb2684b5fca1
Alexander Truyaert <alexander.truyaert@outlook.com>
2022-01-30 21:14:35 +0100

    Added solution rotate array

---
+++ rotate-array/solution.ts
@@ -0,0 +1,9 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function rotate(nums: number[], k: number): void {
+    for (let i = 0; i < k; i++) {
+        let number = nums.pop();
+        nums.unshift(number);
+    }
+};

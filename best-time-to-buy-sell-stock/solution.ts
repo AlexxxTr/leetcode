763972c887e506e659aeeb7f60156c71e75d3005
Alexander Truyaert <alexander.truyaert@outlook.com>
2022-02-01 15:33:00 +0100

    Added solution best time buy sell stock

---
+++ best-time-to-buy-sell-stock/solution.ts
@@ -0,0 +1,10 @@
+function maxProfit(prices: number[]): number {
+    let max: number = 0;
+    let ans: number = 0;
+    for (let i = prices.length - 1; i >= 0; i--) {
+        ans = Math.max(ans, max - prices[i]);
+
+        if (prices[i] > max) max = prices[i]
+    }
+    return ans;
+};

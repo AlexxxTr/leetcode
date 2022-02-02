1bc01d89fa336bfd2491564dd9325609faaf11f1
Alexander Truyaert <alexander.truyaert@outlook.com>
2022-02-02 15:20:48 +0100

    Added cleaner solution for house robber

--- house-robber/solution.ts
+++ house-robber/solution.ts
@@ -1,7 +1,7 @@
 function rob(nums: number[]): number {
     if (nums.length === 1) return nums[0];
     for (let i = 2; i < nums.length; i++) 
-        nums[i] += Math.max(...nums.slice(0, i - 1))
+        nums[i] = Math.max(nums[i - 1], nums[i - 2] + nums[i]);
     
-    return Math.max(...nums)
+    return nums[nums.length - 1];
 };

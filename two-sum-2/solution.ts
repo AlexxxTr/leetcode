125aae35261ff286557ebb8ee76b180c14836cf0
Alexander Truyaert <alexander.truyaert@outlook.com>
2022-02-06 16:56:22 +0100

    Added solution two sum 2

---
+++ two-sum-2/solution.ts
@@ -0,0 +1,23 @@
+function twoSum(numbers: number[], target: number): number[] {
+    for (let i = 0; i < numbers.length; i++) {
+        let number = numbers[i];
+        let toFind = target - number;
+        let secIndex = search(numbers.slice(i + 1), toFind);
+        if (secIndex !== -1) return [i + 1, secIndex + i + 2]; // Of by one and it is one indexed
+    }
+};
+
+function search(numbers: number[], target: number): number {
+    let start = 0, end = numbers.length - 1;
+    console.log(numbers, target);
+    while (start <= end) {
+        let middle = Math.floor(start + (end - start) / 2);
+        console.log(start, middle, end);
+        console.log(numbers[middle]);
+        if (numbers[middle] === target) return middle;
+        (numbers[middle] > target) ? end = middle - 1 : start = middle + 1;
+    }
+    return -1
+}
+
+console.log(twoSum([2,3,4], 6));

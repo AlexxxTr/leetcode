eaee719c02a0b7a4b83791b9b4ae2db34a2cacdc
Alexander Truyaert <alexander.truyaert@outlook.com>
2022-02-11 09:13:51 +0100

    Added solution max prodcut subarray

---
+++ maximum-product-subarray/solution.ts
@@ -0,0 +1,16 @@
+function maxProduct(nums: number[]): number {
+    let prevMax: number, prevMin: number, result: number;
+    prevMax = prevMin = result = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        let curMax = Math.max(nums[i] * prevMax, nums[i], nums[i] * prevMin);
+        let curMin = Math.min(nums[i] * prevMin, nums[i], nums[i] * prevMax);
+
+        prevMax = curMax;
+        prevMin = curMin;
+
+        result = Math.max(curMax, result);
+    }
+    return result;
+}
+
+console.log(maxProduct([-2, 0, -1]));

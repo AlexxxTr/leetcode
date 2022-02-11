2dd4cd14bfe74d905f0903b7f46b4cbe38786c09
Alexander Truyaert <alexander.truyaert@outlook.com>
2022-02-11 12:17:24 +0100

    Added mine and someone elses solutiont best sightseeing pair

---
+++ best-sightseeing-pair/solutiont.ts
@@ -0,0 +1,23 @@
+function maxScoreSightseeingPair(values: number[]): number {
+    const table = Array(values.length).fill(0);
+    for (let i = 0; i < values.length; i++) {
+        for (let j = i + 1; j < values.length; j++) {
+            let score = values[i] + values[j] + i - j;
+            if (score > table[i]) table[i] = score;
+        }
+    }
+    return Math.max(...table);
+}
+
+function maxScoreSightseeingPair2(values: number[]): number {
+    let prevMax = values[0], max = 0;
+    for(let i = 1; i < values.length; i++) {
+        prevMax--;
+        max = Math.max(max, prevMax + values[i]);
+        prevMax = Math.max(prevMax, values[i]);
+    }
+    return max;
+}
+
+
+console.log(maxScoreSightseeingPair([1,2]));

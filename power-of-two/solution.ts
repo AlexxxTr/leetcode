3aeec6b69195b1f6b8edd4f5ed6bddd9c1ff8f72
Alexander Truyaert <alexander.truyaert@outlook.com>
2022-02-15 10:28:03 +0100

    Added solution power of two

---
+++ power-of-two/solution.ts
@@ -0,0 +1,8 @@
+function isPowerOfTwo(n: number): boolean {
+    if (n <= 0) return false;
+    while (n !== 1) {
+        n = n / 2;
+        if (n % 2 !== 0 && n !== 1) return false;
+    }
+    return true;
+};

6a1e999af58e474a77beebcf4c7548e08e13ffaa
Alexander Truyaert <alexander.truyaert@outlook.com>
2022-02-15 10:31:34 +0100

    Added solution to check for all powers easily

--- power-of-two/solution.ts
+++ power-of-two/solution.ts
@@ -1,8 +1,12 @@
 function isPowerOfTwo(n: number): boolean {
+    return isPowerOfN(n, 2);
+};
+
+function isPowerOfN(n: number, power: number): boolean {
     if (n <= 0) return false;
     while (n !== 1) {
-        n = n / 2;
-        if (n % 2 !== 0 && n !== 1) return false;
+        n = n / power;
+        if (n % power !== 0 && n !== 1) return false;
     }
     return true;
-};
+}

4227779046c77086a0f5ff6d1c983dacb6092015
Alexander Truyaert <alexander.truyaert@outlook.com>
2022-02-16 15:33:24 +0100

    Added solution pascal triangle 2

---
+++ pascal-triangle-2/solution.ts
@@ -0,0 +1,17 @@
+function getRow(rowIndex: number): number[] {
+    if (rowIndex === 0) return [1];
+
+    const currentLineSize = rowIndex + 1;
+    const previousLineSize = currentLineSize - 1;
+
+    const currentLine = [];
+
+    const previousLine = getRow(rowIndex - 1);
+
+    for (let numIndex = 0; numIndex < currentLineSize; numIndex++) {
+        const leftCoefficient = (numIndex - 1) >= 0 ? previousLine[numIndex - 1] : 0;
+        const rightCoefficient = numIndex < previousLineSize ? previousLine[numIndex] : 0;
+        currentLine[numIndex] = leftCoefficient + rightCoefficient;
+    }
+
+    return currentLine;
